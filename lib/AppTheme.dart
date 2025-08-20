4cdeb1a4a678c3d556c05c17fc2d9c74b9f417da
Mihai Dragusin <33466422+MNDragusin@users.noreply.github.com>
2025-08-21 01:22:26 +0300

    default theme data

---
+++ lib/AppTheme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final defaultTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 131, 50, 0)),
+  textTheme: GoogleFonts.latoTextTheme()
+);
